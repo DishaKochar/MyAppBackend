9e7ff2a974c9c9541b1c4f7e8d09b7102b55fe1f
DishaKochar <kochardisha15@gmail.com>
2022-12-02 19:03:33 +0530

    db changes

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const url = 'mongodb://localhost/MyApp'
+//const url = 'mongodb://localhost/MyApp'
+const url = 'mongodb+srv://disha:12345@todolistcluster.y8wxqsx.mongodb.net/MyApp?retryWrites=true&w=majority'
+
 mongo.connect(url)
 
 mongo.connection.on('open',function(){
